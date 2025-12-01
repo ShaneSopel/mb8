cadedb204250cc4a8809fef8fd931ea6964fdec5
Osuji <weezdomosuji@gmail.com>
2025-12-01 16:14:14 +0100

    fix: truncate filenames to 8 chars (#52)

--- crates/cli/src/main.rs
+++ crates/cli/src/main.rs
@@ -225,7 +225,7 @@ fn map_key_to_char(key: Key, shift: bool) -> Option<u8> {
 
     Some(ch)
 }
-
+#[allow(clippy::too_many_lines)]
 fn run_vm(kernel: PathBuf, user: Vec<PathBuf>, seed: Option<u16>) {
     let Ok(rom) = std::fs::read(kernel) else {
         return;
@@ -262,7 +262,13 @@ fn run_vm(kernel: PathBuf, user: Vec<PathBuf>, seed: Option<u16>) {
         fs[zero_block_start + 2] = size as u8;
 
         let chars = name.as_encoded_bytes();
-        // TODO: check if file name size is less than 8
+        if chars.len() > 8 {
+            eprintln!(
+                "Error: File name {} is too long. Max 8 characters allowed.",
+                name.to_string_lossy()
+            );
+            return;
+        }
         for (i, c) in chars.iter().enumerate() {
             fs[zero_block_start + 3 + i] = *c;
         }
